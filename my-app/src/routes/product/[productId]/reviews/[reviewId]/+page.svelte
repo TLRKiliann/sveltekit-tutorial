0af4b59afc46d35b73336d893aae9f2f6a191799
TLRKiliann <philogenie@protonmail.com>
2023-04-30 12:23:59 +0200

    dynamic route created for reviews into productId

---
+++ my-app/src/routes/product/[productId]/reviews/[reviewId]/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import {page} from '$app/stores';
+    const {productId, reviewId} = $page.params;
+</script>
+
+<h1>Reviews & productId</h1>
+<h2>productId: {productId} & reviewId: {reviewId}</h2>

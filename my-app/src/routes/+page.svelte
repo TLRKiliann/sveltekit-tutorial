2fc5edf5a26c7ae58c65d489d688e67b1ca7fbe8
TLRKiliann <philogenie@protonmail.com>
2023-05-15 14:23:03 +0200

    links added

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,1 +1,10 @@
 <h1>Welcome to SvelteKit</h1>
+
+<div>
+	<a href="/marketing">Marketing EN</a>
+	<a href="/marketing/es">Marketing ES</a>
+	<a href="/marketing/fr">Marketing FR</a>
+	<a href="/products">Products</a>
+	<a href="/doc">Doc</a>
+	<a href="/about">About</a>
+</div>

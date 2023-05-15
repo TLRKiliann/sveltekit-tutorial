6a335d7fb51fc94c6109de60eb248081d4e7e1fb
TLRKiliann <philogenie@protonmail.com>
2023-05-15 14:23:30 +0200

    product replaced by products

---
+++ my-app/src/routes/products/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let productPerCent = 100;
+</script>
+
+<a href="/">Home</a>
+
+<h1>Product Title</h1>
+
+<h1>Product details by ID</h1>
+<a href="/products/1">Product 1 :</a>
+<a href="/products/2">Product 2 :</a>
+<a href="/products/3">Product 3 :</a>
+<a href={`/products/${productPerCent}`}>
+	Product: {productPerCent}
+</a>

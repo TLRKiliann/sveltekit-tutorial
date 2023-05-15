ad0b4404425574107ae3241ce35a58c13c780033
TLRKiliann <philogenie@protonmail.com>
2023-05-15 23:21:18 +0200

    api created to communicate with lib/comments.ts

---
+++ my-app/src/lib/comments.ts
@@ -0,0 +1,19 @@
+type CommentProps = {
+  id: number;
+  text: string;
+};
+
+export const comments: CommentsProps = [
+  {
+    id: 1,
+    text: 'This is the first comment1'
+  },
+  {
+    id: 2,
+    text: 'This is the first comment2'
+  },
+  {
+    id: 3,
+    text: 'This is the first comment3'
+  },
+];

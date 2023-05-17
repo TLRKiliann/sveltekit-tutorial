3ecaebdbcf87418b14a5caaa45d62533a3153aad
TLRKiliann <philogenie@protonmail.com>
2023-05-17 15:35:08 +0200

    error manager added

---
+++ sk-loading-data/src/routes/products/[productId]/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { page } from '$app/stores'; 
+</script>
+
+<h1>
+	{$page.error.message}
+</h1>
+<p>
+	{$page.error.hint}
+</p>
--- sk-loading-data/src/routes/products/[productId]/+page.server.js
+++ sk-loading-data/src/routes/products/[productId]/+page.server.js
@@ -1,8 +1,16 @@
+import {error} from '@sveltejs/kit'
+
 export const load = async (serverLoadEvent) => {
 	console.log("Load function called in page.server.js");
 	const { fetch, params } = serverLoadEvent;
 	//console.log({ params, url, route: route.id });
 	const { productId } = params;
+	if (productId > 3) {
+		throw error(404,{
+			message: 'Product not found',
+			hint: 'try a different product'
+		});
+	}
 	const title = "Product details second title";
 	const notification = "End of season sale! 50% off";
 	const res = await fetch(`http://localhost:4000/products/${productId}`);
